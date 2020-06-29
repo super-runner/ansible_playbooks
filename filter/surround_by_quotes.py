ab16c5b149b6255d67fa1bedab57d8da3cc27eec
Jason <88933306@qq.com>
2020-06-29 14:08:05 +0000

    optimized structures.created create_mai_file.yml which builds main.yml under a role

--- filter/surround_by_quotes.py
+++ filter/surround_by_quotes.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def surround_by_quote(a_list):
-    return ['"%s"' % an_element for an_element in a_list]
+    return ['"%s"' %x for x in a_list]
 
 class FilterModule(object):
     def filters(self):
         return {'surround_by_quote': surround_by_quote}
+
+if __name__ == '__main__':
+  print ("{}".format(surround_by_quote(['jason','becky'])))
+
